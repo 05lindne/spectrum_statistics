aac7b328b054e3aa66520489c6660fe55b5b9c64
05lindne <05lindne@hotmail.com>
2015-09-17 18:16:09 +0200

    simple argparse; g2 spots indicated in red for 3 column input data

--- line_statistics.py
+++ line_statistics.py
@@ -11,6 +11,8 @@
 from sys import argv
 import numpy as np
 from matplotlib import pyplot as plt
+import argparse
+import collections
 
 # define appearance
 color_scale = 'CMRmap'
@@ -19,26 +21,25 @@ tick_fontsize = 23
 label_fontsize = 23
 title_fontsize = 23
 
-g2_flag = 0
-# argv is your commandline arguments, argv[0] is your program name, so skip it
-out_filename = argv[1]
-g2_file = 0
 
-if len(argv) < 2:
-    print "Usage: <out_filename> <in_filename1> <in_filename2> <in_filename...> (optional:) <g2> <g2_file>"
+parser = argparse.ArgumentParser()
+parser.add_argument('out_filename', help = 'name for output files; without extension')
+parser.add_argument('in_files', nargs = '*', help = 'input filenames')
+parser.add_argument("-g2", action = "store_true", help = "indicate g2 data") 
+parser.add_argument("-v", "--verbose", action = "store_true", help = "increase output verbosity") 
 
-if "g2" in argv:
-    print "indicating g2 spots in scatter plot"
-    g2_flag = 1
-    g2_listposition = argv.index('g2')
-    in_files = argv[2:g2_listposition]
-    g2_file = argv[g2_listposition+1:]
-else:
-    in_files = argv[2:]
+args = parser.parse_args() 
+if args.verbose: 
+    print 'verbose output'
+    if args.g2: 
+        print 'data having g2 dip will be indicated in plot'
+print parser.parse_args()
+
 
 
 line_position_all = []
 line_width_all = []
+g2_exist_all = []
 
 legend_entries = []
 
@@ -46,13 +47,14 @@ legend_entries = []
 
 
 #read in data
-for in_file in in_files, g2_file:
+for in_file in args.in_files:
     print('--> reading file ' + in_file) #print out the filename we are currently processing
 
-    line_position, line_width = np.loadtxt( in_file, delimiter="\t", usecols=(0, 1), unpack=True)
+    line_position, line_width, g2_exist = np.loadtxt( in_file, usecols=(0, 1, 2), unpack=True)
 
     line_position_all.append(line_position)
     line_width_all.append(line_width)
+    g2_exist_all.append(g2_exist)
 
     # prepare legend entries from file name
     file_parts = in_file.split('/')
@@ -61,6 +63,10 @@ for in_file in in_files, g2_file:
     legend_entry = last_part_split[len(last_part_split)-2] # take item before last dot
 
     legend_entries.append(legend_entry)
+
+
+
+
 
 
 
@@ -92,11 +98,11 @@ plt.tight_layout()
 plt.legend(prop={'size':legend_fontsize})
 plt.grid(True)
 
-plt.savefig( (out_filename +'_width.png'))
-plt.savefig( (out_filename +'_width.pdf'))
+plt.savefig( (args.out_filename +'_width.png'))
+plt.savefig( (args.out_filename +'_width.pdf'))
 
-print '--> saved figure ' + out_filename +'_width.png'
-print '--> saved figure ' + out_filename +'_width.pdf'
+print '--> saved figure ' + args.out_filename +'_width.png'
+print '--> saved figure ' + args.out_filename +'_width.pdf'
 
 plt.show()
 
@@ -131,17 +137,18 @@ plt.tight_layout()
 legend = plt.legend(prop={'size':legend_fontsize})
 plt.grid(True)
 
-plt.savefig( (out_filename +'_position.png'))
-plt.savefig( (out_filename +'_position.pdf'))
+plt.savefig( (args.out_filename +'_position.png'))
+plt.savefig( (args.out_filename +'_position.pdf'))
 
-print '--> saved figure ' + out_filename +'_position.png'
-print '--> saved figure ' + out_filename +'_position.pdf'
+print '--> saved figure ' + args.out_filename +'_position.png'
+print '--> saved figure ' + args.out_filename +'_position.pdf'
 
 plt.show()
 
 
 
-
+# for item in g2_exist_all:
+    # legend_entries.append("g2")
 
 ax = plt.subplot() # Defines ax variable by creating an empty plot; needed for tuning appearance, e.g. axis ticks font
 
@@ -150,9 +157,16 @@ coloring = plt.get_cmap(color_scale, len(line_position_all))
 
 plot_list = []
 # plot
-for index, (line, width) in enumerate( zip(line_position_all, line_width_all) ):
-    plot = plt.scatter(line, width, color='k', lw=1, s=20, marker='o', facecolor=coloring(index))
+for index, (line, width, g2_exist) in enumerate( zip(line_position_all, line_width_all, g2_exist_all) ):
+    plot = plt.scatter(line, width, color='k', lw=1, s=20, marker='o', facecolor=coloring(index), label = legend_entries[index])
     plot_list.append(plot)
+
+    line_position_g2 = [p for p,g in zip(line, g2_exist) if g == 1]
+    line_width_g2 = [w for w,g in zip(width, g2_exist) if g == 1]
+
+    plot = plt.scatter(line_position_g2, line_width_g2, color='k', lw=1, s=20, marker='o', facecolor="r", label = 'g2')
+    plot_list.append(plot)
+
 
 
 plt.xlabel('Position (nm)', fontsize = label_fontsize)
@@ -164,13 +178,18 @@ for label in (ax.get_xticklabels() + ax.get_yticklabels()):
     label.set_fontsize(tick_fontsize)
 
 plt.tight_layout()
-legend = plt.legend(reversed(plot_list), reversed(legend_entries), prop={'size':legend_fontsize})
+# legend = plt.legend(reversed(plot_list), reversed(legend_entries), prop={'size':legend_fontsize})
+# legend = plt.legend(collections.OrderedSet(plot_list), collections.OrderedSet(legend_entries), prop={'size':legend_fontsize})
+# legend = plt.legend(plot_list, legend_entries, prop={'size':legend_fontsize})
+# legend1 = plt.legend(plot_list, legend_entries, prop={'size':legend_fontsize})
+# ax = plt.gca().add_artist(legend1)
+legend2 = plt.legend(prop={'size':legend_fontsize})
 plt.grid(True)
 
-plt.savefig( (out_filename +'_width_position.png'))
-plt.savefig( (out_filename +'_width_position.pdf'))
+plt.savefig( (args.out_filename +'_width_position.png'))
+plt.savefig( (args.out_filename +'_width_position.pdf'))
 
-print '--> saved figure ' + out_filename +'_width_position.png'
-print '--> saved figure ' + out_filename +'_width_position.pdf'
+print '--> saved figure ' + args.out_filename +'_width_position.png'
+print '--> saved figure ' + args.out_filename +'_width_position.pdf'
 
 plt.show()
